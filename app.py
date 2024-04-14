ad93d72615feb91504b23db0a7c2434b88f3f783
Mac Myller da Silva Carlos <myllersilva1310@gmail.com>
2024-04-14 23:11:12 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+
+from routes_api import *
+from routes_templates import *
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
