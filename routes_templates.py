b24e75311b0f71db1fb94880c1f2aa578b39f0d3
Mac Myller da Silva Carlos <myllersilva1310@gmail.com>
2024-04-15 21:24:27 +0000

    create: template error.html

--- routes_templates.py
+++ routes_templates.py
@@ -1,4 +1,5 @@
 from flask import jsonify, render_template, request
+import requests
 from app import app
 from utils import get_models_dataframe
 from config import INSTANCIA_TEMPLATE
@@ -29,9 +30,8 @@ def get_classify_chagas():
     # TODO: receber do banco de dados os modelos disponiveis
     modelos = modelos_chagas
     # TODO: receber do banco de dados o exemplo de instancia para o modelo especifico
-    instancia = INSTANCIA_TEMPLATE
     return render_template(
-        "/classify/chagas.html", modelos=modelos, instancia=instancia
+        "/classify/chagas.html", modelos=modelos, instancia=INSTANCIA_TEMPLATE
     )
 
 
@@ -43,18 +43,21 @@ def post_classify_chagas():
         if key != "model":
             instancia[key] = float(value)  # converter todos os valores para float
 
-    with app.test_client() as c:
-        response = c.post(
-            f"{base_url_api}/chagas",
-            json={"model": selected_model, "instancia": instancia},
+    
+    response = requests.post(
+        f"{base_url_api}/chagas",
+        json={"model": selected_model, "instancia": instancia},
+    )
+    if response.status_code == 200:
+        return render_template(
+            "/result/chagas.html",
+            model=selected_model,
+            instancia=instancia,
+            response_data=response.json(),
         )
-
-        if response.status_code == 200:
-            return render_template(
-                "/result/chagas.html",
-                model=selected_model,
-                instancia=instancia,
-                response_data=response.json,
-            )
-        else:
-            return response.json, response.status_code
+    else:
+        return render_template(
+            "error.html",
+            response_json=response.json(),
+            status_code=response.status_code,
+        )
